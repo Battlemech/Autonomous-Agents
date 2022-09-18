b55bac88a1893efcb5afb6e2cce8d84bb57948cc
erik.fischer98 <erik.fischer98@win.tu-berlin.de>
2022-09-18 15:42:17 +0200

    train model in true loop

--- franka_move/franka_move_task.py
+++ franka_move/franka_move_task.py
@@ -37,7 +37,7 @@ class FrankaMoveTask(BaseTask):
         self._max_speed = 5.0
         self._goal_tolerance = 0.2
         self._max_target_distance = 1.0
-        self._reset_after = 200
+        self._reset_after = 300
 
         # values used for defining RL buffers
         self._num_observations = 24 # 9 rotor states [0, 2*Pi] + 9 rotor velocities + 3 * current coordinates of finger + 3 * goal coordinates
@@ -190,27 +190,43 @@ class FrankaMoveTask(BaseTask):
         return self.obs
 
     def calculate_metrics(self) -> None:
+        # calculate distances
         distances = self.calculate_distances()
         distance_metric = -(distances ** 2)
 
-        # give positive reward if goal was reached
+        # give positive reward if goal was reached 
+        #todo: more efficient with where, how?
         for i in range(len(distances)):
             if(distances[i] <= self._goal_tolerance):
-                distance_metric = ((self._reset_after - self.timestep_count[i]) / self._reset_after) * 10
+                distance_metric[i] = 2
+
+        # save distances for IsDone:
+        self.distances = distances
 
         return distance_metric.item()
         # action_metric = -(torch.sum(torch.abs(self.prev_actions - self.actions), dim=1) / (self._num_actions * 4))
         # return (distance_metric + action_metric).item()
 
     def is_done(self) -> None:
-        # reset the robot if finger is in target region
-        resets_goal = torch.where(self.calculate_distances() <= self._goal_tolerance, 1, 0)
-
+        
         # reset the robot if too many timespeps have passed in attempt to reach goal
-        resets_failure = torch.where(self.timestep_count >= self._reset_after, 1, 0)
+        timelimit_reached = torch.where(self.timestep_count >= self._reset_after, 1, 0)
+
+        # if robots reached time limit
+        if(torch.any(timelimit_reached)):
+            # check if goal was reached
+            # use self.distances since they were already calculated in calculate_metrics
+            goal_reached = torch.where(self.distances <= self._goal_tolerance, 1, 0)
+
+            # and track it accordingly
+            for i in range(self.num_envs):
+                if goal_reached[i] == 1:
+                    self.target_reached_count += 1
+                else:
+                    self.failure_count += 1
 
         # get previous resets from physx errors
-        resets = torch.where(resets_goal + resets_failure + self.resets >= 1, 1, 0)
+        resets = torch.where(timelimit_reached + self.resets >= 1, 1, 0)
 
         # reset timestep count of reset robots to 0
         self.timestep_count = (torch.ones((self.num_envs, 1)) - (resets)) * self.timestep_count
--- franka_move/main.py
+++ franka_move/main.py
@@ -1,19 +1,45 @@
 from omni.isaac.gym.vec_env import VecEnvBase
-env = VecEnvBase(headless=False)
+env = VecEnvBase(headless=True)
 
 from franka_move_task import FrankaMoveTask
 task = FrankaMoveTask(name="Franka")
 env.set_task(task, backend="torch")
 
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
 
-timesteps = 1000000
+from os.path import exists
 
-# create agent from stable baselines
-model = PPO(
+timesteps = 100000
+path = "ppo_franka"
+
+# log success rate to tensor board
+class TensorBoardCallback(BaseCallback):
+    def __init__(self) -> None:
+        super(TensorBoardCallback, self).__init__()
+
+    def _on_step(self) -> bool:
+        if task.target_reached_count == 0 or task.failure_count == 0:
+                return True
+
+        # reset target_reached count and failure count if sum gets to high -> Accuratly display new attempts
+        if task.target_reached_count + task.failure_count >= 100:
+                task.target_reached_count = task.target_reached_count / 25
+                task.failure_count = task.failure_count / 25
+
+        self.logger.record('Success rate', task.target_reached_count / (task.target_reached_count + task.failure_count))
+        return True
+
+
+# try loading old model. OnFail: create new one
+if exists(path):
+        model = PPO.load(path)
+else:
+        # create agent from stable baselines
+        model = PPO(
         "MlpPolicy",
         env,
-        n_steps=2000,
+        n_steps=1024,
         batch_size=1000,
         n_epochs=20,
         learning_rate=0.001,
@@ -25,7 +51,9 @@ model = PPO(
         verbose=1,
         tensorboard_log="./franka_tensorboard"
 )
-model.learn(total_timesteps=timesteps)
-model.save("ppo_franka")
+
+while True:
+        model.learn(total_timesteps=timesteps, callback=TensorBoardCallback())
+        model.save("ppo_franka")
 
 env.close()
